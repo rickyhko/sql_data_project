1dbb837d3b641da1b55d0749fcd876dd3f475051
rickyhko <rickyhko@gmail.com>
2026-07-21 18:48:43 +0700

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,3 +1,10 @@
+-- drop and recreate datawarehouse database 
+-- careful because it will delete all database permanantly
+
+DROP DATABASE IF EXISTS datawarehouse;
+
+-------------------------------------------------------------------
+
 CREATE DATABASE datawarehouse;
 
 USE datawarehouse;
